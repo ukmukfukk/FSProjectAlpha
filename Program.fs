60fb921d2d709346ce6d044433c9e91925dec0fe
ukmukfukk <ukmukfukk@gmail.com>
2025-05-04 22:27:45 +0200

    Day3 finished

--- Program.fs
+++ Program.fs
@@ -4,3 +4,4 @@ runpuzzle Day1.solve1 "Samples/input11.txt" "Day1 puzzle 1"
 runpuzzle Day1.solve2 "Samples/input11.txt" "Day1 puzzle 1" 
 
 runpuzzle Day3.solve1 "Samples/input3.txt" "Day3 puzzle 1"
+runpuzzle Day3.solve2 "Samples/input3.txt" "Day3 puzzle 2"
--- TestSamples.fsx
+++ TestSamples.fsx
@@ -11,3 +11,6 @@ evaluate Day1.solve2 "Samples\Sample1.txt" expected12 "Day1 puzzle 2"
 
 let expected21 = 2
 evaluate Day3.solve1 "Samples\Sample3.txt" expected21 "Day3 puzzle 1"
+
+let expected22 = 4
+evaluate Day3.solve2 "Samples\Sample3.txt" expected22 "Day3 puzzle 2"
